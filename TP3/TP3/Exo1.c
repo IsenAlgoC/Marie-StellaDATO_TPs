8bcb5e425eb0c1e3541bad412dc35b3c9869b42b
Marie-Stella DATO <marie-stella.dato@isen.yncrea.fr>
2020-10-08 01:23:46 +0200

    Exo1

---
+++ TP3/TP3/Exo1.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define carre(x) x*x
+
+int main()
+{
+	int Nb = 5;
+	int Nb2 = 6;
+	printf("c'est %d", carre(Nb));   // Ca donne 25
+	printf("\nc'est %d", carre((Nb + 1))); // Ca donne 11  || 
+	printf("\nc'est %d", carre((Nb + 2))); //17    ||  
+
+	//Le pb : la priorité des calculs
+}
