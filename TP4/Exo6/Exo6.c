162b309087ffa9ec8cb2525cbcb2467498b579f2
Marie-Stella DATO <marie-stella.dato@isen.yncrea.fr>
2020-10-17 20:08:56 +0200

    ;

--- TP4/Exo6/Exo6.c
+++ TP4/Exo6/Exo6.c
@@ -32,14 +32,14 @@ int main() {
 			Curseur = &tableau[j];
 			position = j;
 			position1 = j;
-			printf("\n La valeur %d a été trouvé en %d ", nbre, position);
+			printf("\n La valeur %d a été trouvé en %d ", nbre, position+1);
 
 			for (J = j; J < 100; J++) {		//Je reprends ma boucle à J=j
 				if (tableau[J] == nbre) {
 					Curseur = &tableau[J];
 					position = J;
-					if (position != position1) { printf("puis en %d ", position); }		//Si la nouvelle position est différente de la première
-																						//afficher la position
+					if (position != position1) { printf("puis en %d ", position+1); }	//Si la nouvelle position est différente de la première
+																						//afficher la position   || le position + 1 c'est pour faciliter la lecture à l'utilisateur
 				}
 			}
 			j = 99;  //Je force la fin de ma boucle
